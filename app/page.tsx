c22363a01e3f218c88f431b029d1be4a72b8e861
rafovic <a.radovic13@gmail.com>
2026-02-01 23:57:22 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>PadelBuddy</h1>
+      <p>🎾 L’app est bien en ligne !</p>
+      <p>Si tu vois ce message, tout fonctionne.</p>
+    </main>
+  );
+}
